927062faac00734a5eec185a8946954ea461d28c
namith-1 <nrnr1nrnr11@gmail.com>
2025-10-08 01:02:56 +0530

    Configured DataBase
    configured Database(mongoDB) and added a moongose model for a collect

---
+++ backend/server.js
@@ -0,0 +1,11 @@
+import express from "express";
+import dotenv from "dotenv";
+import {connectDB} from "./config/db.js"
+dotenv.config();
+
+const app = express();
+
+app.listen(3000,()=>{
+ connectDB();
+ console.log("running at port https://localhost:3000");
+});
